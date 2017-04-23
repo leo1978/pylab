c9c3a372a69c47feaddbbafcead06b874fc9c762
Leo <leo1978@gmail.com>
2017-04-23 16:18:02 -0400

    upddate

--- timelines.py
+++ timelines.py
@@ -1,6 +1,25 @@
-def plotTimeLines(ids, start, end):
-    y = { ids[i]:i for i in range(len(ids))}
+from matplotlib.pyplot import *
+
+T = {'id':['a','a','b','b'],'start':[1,5,1,4], 'end':[2,10,3,10]}
+
+def partition(*data):
+    p = {}
+    for key, *values in zip(*data):
+        if key in p:
+            p[key].append(values)
+        else:
+            p[key] = [values]
+    return list(p.keys()), list(p.values())
     
-    for id, s, t in zip(ids, start, end):       
-        plot([s,t],[y[id],y[id]],'black')
-        
+    
+    
+def plotTimeLines(ids, start, end):
+    idset, segments = partition(ids, start, end)
+    y = { idset[i]:i for i in range(len(idset))}
+    
+    for id, segment in zip(idset, segments):
+        for s, t in segment:       
+            l, = plot([s,t],[y[id],y[id]])
+            setp(l, color='black', label=str(id))
+
+    legend()
